e5e05697716ffbf61a095ccace23f7feb37b8872
JalexSM <jsamayoam9@miumg.edu.gt>
2026-02-16 15:51:28 -0600

    cambio de nombre de variable e ingreso de datos

--- Main.java
+++ Main.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
 
 public class Main {
 	
-	public static void main(String[]args) {
+	public static void main(String[]args) { 
 		
 		 Scanner sc = new Scanner(System.in);
-		
-	int valorint; 	
-	double valordouble;
-	String valorstring;
-	boolean valorboleano; 	
+		 
+		 int edadUsuario;
+		 String nombreUsuario;
+		 double estaturaUsuario;
+		 boolean estadoLaboral;
+		 
 		
 	
-		
+	        System.out.println("ingrese su nombre ");
+	        nombreUsuario = sc.nextLine();
+	        System.out.println("ingrese su edad ");
+	        edadUsuario = sc.nextInt();
+	        System.out.println("ingrese su estatura ejem.(1.78)");
+	        estaturaUsuario = sc.nextDouble();
+	        System.out.println("trabaja actualmente (true/false)");
+	        estadoLaboral = sc.nextBoolean();
+	       
 	}
-	 
-}
+}	
