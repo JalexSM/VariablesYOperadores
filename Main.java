dc2efa0dd055652a45d99395811c12cb708b2af6
JalexSM <jsamayoam9@miumg.edu.gt>
2026-02-16 15:11:04 -0600

    declaracion de variables

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+	
+	public static void main(String[]args) {
+		
+		 Scanner sc = new Scanner(System.in);
+		
+	int valorint; 	
+	double valordouble;
+	String valorstring;
+	boolean valorboleano; 	
+		
+	
+		
+	}
+	 
+}
