3eaad322883e898c6be67cabb4797a839942221f
JalexSM <jsamayoam9@miumg.edu.gt>
2026-02-16 16:40:11 -0600

    correciones en if boleano y operaciones con enteros

--- Main.java
+++ Main.java
@@ -6,6 +6,10 @@ public class Main {
 		
 		 Scanner sc = new Scanner(System.in);
 		 
+		 
+		 // 1. DECLARACION Y USO DE VARIABLES
+		 
+		  
 		 int edadUsuario;
 		 String nombreUsuario;
 		 double estaturaUsuario;
@@ -22,5 +26,25 @@ public class Main {
 	        System.out.println("trabaja actualmente (true/false)");
 	        estadoLaboral = sc.nextBoolean();
 	       
+	        System.out.println("usted tiene " + edadUsuario + "años, se llama " + nombreUsuario + ", mide  " + estaturaUsuario + "m "  );
+		       if (estadoLaboral == true) {
+		    	   System.out.println("usted actualmente se encuentra laborando");
+		    	   
+		       }else {
+		    	   System.out.println("usted se encuentra sin empleo ");
+		       }
+		       
+		     //2 OPERACIOBES MATEMATICAS
+		       
+		       System.out.println(" ** operaciones matematicas ** ");
+		       System.out.println(" ingresa el primer numero ");
+		       int primernumero = sc.nextInt();
+		       System.out.println(" ingresa el segundo numero ");
+		       int segundonumero = sc.nextInt();
+		      
+		       System.out.println("la suma de los numeros es "+  (primernumero+segundonumero) );
+		       System.out.println("la resta de los numeros es "+  (primernumero-segundonumero) );
+		       System.out.println("la multiplicacion de los numeros es "+  (primernumero*segundonumero) );
+		       System.out.println("la division de los numeros es "+  (primernumero/segundonumero) );
 	}
 }	
